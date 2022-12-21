2c2db6bd46afabb6b02717a49636899d36113b82
Benitez, Isidoro <isidoro.benitez@philotech.es>
2022-12-21 16:32:08 +0100

    add table

--- PhTrack/src/app/dashboard/components/user/user.component.ts
+++ PhTrack/src/app/dashboard/components/user/user.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { ViewChild} from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
@@ -19,19 +22,65 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
-
 /**
  * @title Basic use of `<table mat-table>`
  */
 @Component({
   selector: 'user-app',
-  styleUrls: ['user.component.scss'],
+  styleUrls: ['user.component.css'],
   templateUrl: 'user.component.html',
 })
 export class UserComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource;
+  displayedColumns = [];
+  @ViewChild(MatSort) sort: MatSort;
+
+  /**
+   * Pre-defined columns list for user table
+   */
+  columnNames = [{
+    id: 'position',
+    value: 'No.',
+
+  }, {
+    id: 'name',
+    value: 'Name',
+  },
+    {
+      id: 'weight',
+      value: 'Weight',
+    },
+    {
+      id: 'symbol',
+      value: 'Symbol',
+    }];
+
+  ngOnInit() {
+    this.displayedColumns = this.columnNames.map(x => x.id);
+    this.createTable();
+  }
+
+  createTable() {
+    let tableArr: Element[] = [{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    ];
+    this.dataSource = new MatTableDataSource(tableArr);
+    this.dataSource.sort = this.sort;
+  }
 }
+
+
+export interface Element {
+  position: number,
+  name: string,
+  weight: number,
+  symbol: string
+}
+
 
 /**  Copyright 2022 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
